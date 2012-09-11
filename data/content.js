ab7ce83f473893a6b9d5bd55c93410c93a2a7816
C. Scott Ananian <cscott@cscott.net>
2012-09-11 15:55:43 -0400

    Hook up JNI module.

--- data/content.js
+++ data/content.js
@@ -1,4 +1,3 @@
 document.documentElement.addEventListener("intent-addon", function(event) {
-    console.log("GOT INTENT-ADDON MESSAGE IN CONTENT SCRIPT: "+JSON.stringify(event.detail));
     self.port.emit("intent-addon", event.detail);
 });
